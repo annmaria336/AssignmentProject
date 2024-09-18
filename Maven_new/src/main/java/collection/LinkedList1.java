6972556df5cbdd4daefc9e016ba42158c719d198
midhu <midhu@192.168.0.11>
2024-09-18 11:46:27 -0400

    MY_FIRST_COMMIT

---
+++ Maven_new/src/main/java/collection/LinkedList1.java
@@ -0,0 +1,16 @@
+package collection;
+
+import java.util.*;
+
+public class LinkedList1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+LinkedList obj=new LinkedList();
+obj.add("Hi");
+obj.add(1);
+obj.add("Hlo");
+System.out.println(obj);
+	}
+
+}
